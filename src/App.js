747e5f2a15750623ebbd4c63e3bc279606ac8b80
susi189371 <susi.susanti@harukaedu.com>
2020-06-06 09:54:22 +0700

    master modal and search bar done

--- src/App.js
+++ src/App.js
@@ -7,24 +7,63 @@ import {
   CardTitle, CardSubtitle, Button, CardLink, ButtonGroup
 } from 'reactstrap';
 import StarRatings from 'react-star-ratings';
-import { ArrowLeft, ChevronDown, Cart3 , ChevronRight} from 'react-bootstrap-icons';
+import { ArrowLeft, ChevronDown, Cart3, ChevronRight } from 'react-bootstrap-icons';
+import Modal from 'react-modal';
+import ReactLiveSearch from 'react-live-search'
 
-
-function add() {
-  alert('oke')
-}
-
+const customStyles = {
+  content: {
+    top: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    zindex: '1000',
+    width: '100%',
+    left: 0,
+    position: 'fixed',
+    bottom: 0
+  }
+};
 
 class App extends Component {
+  state = {
+    number: 10,
+  }
 
   constructor() {
     super();
     this.state = {
-      name: "React"
+      showModal: false,
+      value: '',
+      data: [
+        { label: 'google', value: 1 },
+        { label: 'work', value: 2 },
+        { label: 'great', value: 3 },
+        { label: 'bar', value: 4 },
+        { label: 'foo', value: 5 }
+      ],
     };
   }
 
- 
+  onChange = value => {
+    this.setState({
+      value
+    });
+  };
+
+  onSelect = v => {
+  };
+
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+  handleNumber = number => this.setState({ number });
+  handleOpenModal = this.handleOpenModal.bind(this);
+  handleCloseModal = this.handleCloseModal.bind(this);
 
   data = [
     {
@@ -51,15 +90,12 @@ class App extends Component {
       score: 2.1,
       price: "20.000",
     },
-
   ]
 
   datas = [{ name: 'Jhon', age: 28, city: 'HO' },
   { name: 'Onhj', age: 82, city: 'HN' },
   { name: 'Nohj', age: 41, city: 'IT' }
   ];
-
-
 
   render() {
     return (
@@ -68,7 +104,7 @@ class App extends Component {
           <div className="top-header"><ArrowLeft color="royalblue" size={20} className="mr-3" />
             <div >
               <p className="p-0 m-0 text-left"><small>ALAMAT PENGANTARAN</small></p>
-              <p className="p-0 m-0 text-left"><strong>Tokopedia Tower</strong> <ChevronDown color="red" size={15} /> </p>
+              <p className="p-0 m-0 text-left" onClick={this.handleOpenModal}><strong>Tokopedia Tower</strong> <ChevronDown color="red" size={15} /> </p>
             </div>
           </div>
         </div>
@@ -77,7 +113,7 @@ class App extends Component {
           <a href="#dinner" className="col-md-6">Dinner</a>
         </div>
         <div className="conta">
-          <ul>
+          <ul className="container-top">
             {this.data.map(function (img, x) {
               return <li className="row m-0 mb-2" key={x}>
                 <Card key={x}>
@@ -99,25 +135,30 @@ class App extends Component {
                       <small className="mb-2">by. Kulina - Uptown Lunch</small>
                     </CardText>
                     <span className="text-sub"><strong> Rp.  {img.price}</strong></span>
-                    <Button color="danger" className="float-right pl-3 pr-3 pb-1 pt-1 btn-add-bg" onClick={add}>Add +</Button>
+                    <Button color="danger" className="float-right pl-3 pr-3 pb-1 pt-1 btn-add-bg" >Add +</Button>
                   </CardBody>
                 </Card>
               </li>
             })}
           </ul>
         </div>
-        <div class="footer m-0 p-1 pl-2 pr-2" >
-          <div className="col-md-10 text-align-left">
-            <p className="m-0">5 Items | Rp. 25.000 </p>
-            <small>Termasuk ongkos kirim</small>
-          </div>
-          <div className="col-md-2 cart-position">
-            <Cart3 color="white" size={15} />
-            <ChevronRight color="white" size={12} />
-          </div>
+        <div>
+          <Modal
+            isOpen={this.state.showModal}
+            contentLabel="Minimal Modal Example"
+            style={customStyles}
+          >
+            <button onClick={this.handleCloseModal} className="btn-close-modal" >X</button>
+            <p>Cek makanan yang tersedia di lokasi kamu!</p>
+            <ReactLiveSearch
+              value={this.state.value}
+              onChange={this.onChange}
+              onSelect={this.onSelect}
+              data={this.state.data}
+            />
+          </Modal>
         </div>
       </div>
-
     );
   }
 }
